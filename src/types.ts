672b69983e79b0d759204f48f4f23c69fa1f9847
IAmOscarLiu <karta0989006@gmail.com>
2023-04-08 01:06:42 +0800

    modify types.ts

--- src/types.ts
+++ src/types.ts
@@ -1,13 +1,12 @@
 export interface Hero {
-    id: string;
-    name: string;
-    image: string;
-  }
-  
-  export interface Profile {
-    str: number;
-    int: number;
-    agi: number;
-    luk: number;
-  }
-  
+  id: string;
+  name: string;
+  image: string;
+}
+
+export interface Profile {
+  str: number;
+  int: number;
+  agi: number;
+  luk: number;
+}
